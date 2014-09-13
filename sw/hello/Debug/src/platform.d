f17981010e03af439c6d1234ce882452cd1f024a
Jeramie Lee Vens <vens@linux-1.ece.iastate.edu>
2014-09-13 02:00:56 -0500

    good luck

---
+++ sw/hello/Debug/src/platform.d
@@ -0,0 +1,15 @@
+src/platform.d: ../src/platform.c \
+ ../../bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../bsp/ps7_cortexa9_0/include/xil_types.h ../src/platform_config.h
+
+../../bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
---
+++ sw/vga_test/Debug/src/vga_test.d
@@ -0,0 +1,52 @@
+src/vga_test.d: ../src/vga_test.c \
+ ../../bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../bsp/ps7_cortexa9_0/include/xvtc.h \
+ ../../bsp/ps7_cortexa9_0/include/xvtc_hw.h \
+ ../../bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../bsp/ps7_cortexa9_0/include/xaxivdma.h \
+ ../../bsp/ps7_cortexa9_0/include/xaxivdma_hw.h \
+ ../../bsp/ps7_cortexa9_0/include/xaxivdma_i.h \
+ ../../bsp/ps7_cortexa9_0/include/xdebug.h
+
+../../bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../bsp/ps7_cortexa9_0/include/xvtc.h:
+
+../../bsp/ps7_cortexa9_0/include/xvtc_hw.h:
+
+../../bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../bsp/ps7_cortexa9_0/include/xaxivdma.h:
+
+../../bsp/ps7_cortexa9_0/include/xaxivdma_hw.h:
+
+../../bsp/ps7_cortexa9_0/include/xaxivdma_i.h:
+
+../../bsp/ps7_cortexa9_0/include/xdebug.h:
